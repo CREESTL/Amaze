098f5949278dc4bd6e30e0519e42301a878cbe25
IntegralTeam <@IntegralTeam>
2023-08-02 15:02:44 +0700

    Remove all fees and everything connected with them

--- contracts/Maze.sol
+++ contracts/Maze.sol
@@ -2,27 +2,18 @@
 
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import "./interfaces/IMaze.sol";
 import "./interfaces/ICore.sol";
-
-import "hardhat/console.sol";
 
 /// @title ERC20 token with RFI logic
 /// @dev NOTE: This contract uses the principals of RFI tokens
 ///            for detailed documentation please see:
 ///            https://reflect-contract-doc.netlify.app/#a-technical-whitepaper-for-reflect-contracts
 contract Maze is ERC20, IMaze, Ownable, Pausable {
-    using SafeMath for uint256;
-
-    bool internal inSwap = false;
-
     /// @notice The address of the Core contract
     ICore public core;
 
@@ -32,22 +23,6 @@ contract Maze is ERC20, IMaze, Ownable, Pausable {
     /// @dev Used to convert BPs to percents and vice versa
     uint256 private constant percentConverter = 1e4;
 
-    /// @notice List of whitelisted accounts. Whitelisted accounts do not pay fees on token transfers.
-    mapping(address => bool) public isWhitelisted;
-
-    /// @notice List of DEX pairs for which a sale fee for the buy and sale of tokens will be taken.
-    mapping(address => bool) public isSalePair;
-
-    ISwapRouter public swapRouter;
-    uint24 public poolFee = 3000;
-    address public USDT;
-    address public saleFeeReceiver;
-
-    /// @notice The percentage of transferred tokens to be taken as fee for any token buy or sale in added DEX pairs 
-    ///         Fee is swapped to USDT to marketing address
-    ///         Expressed in basis points
-    uint256 public saleFeeInBP;
-
     /// @notice Checks that account is not blacklisted
     modifier ifNotBlacklisted(address account) {
         require(!core.checkBlacklisted(account), "Maze: Account is blacklisted");
@@ -55,22 +30,13 @@ contract Maze is ERC20, IMaze, Ownable, Pausable {
     }
 
     constructor(
-        address core_,
-        address swapRouter_,
-        address usdt_
+        address core_
     ) ERC20("Maze", "MAZE") {
         require(core_ != address(0), "Maze: Core cannot have zero address");
-        require(swapRouter_ != address(0), "Maze: SwapRouter cannot have zero address");
-        require(usdt_ != address(0), "Maze: USDT cannot have zero address");
         core = ICore(core_);
-        swapRouter = ISwapRouter(swapRouter_);
-        USDT = usdt_;
 
         // Whole supply of tokens is assigned to owner
         _mint(msg.sender, 100_000_000 * 1e18);
-        saleFeeReceiver = msg.sender;
-
-        setSaleFees(300);
     }
 
     /// @notice See {IMaze-approve}
@@ -113,53 +79,6 @@ contract Maze is ERC20, IMaze, Ownable, Pausable {
         return super.decreaseAllowance(spender, subtractedValue);
     }
 
-    /// @notice See {IMaze-setFees}
-    function setSaleFees(uint256 _saleFeeInBP) public whenNotPaused onlyOwner {
-        require(_saleFeeInBP < 1e4, "Maze: Sale fee too high");
-        saleFeeInBP = _saleFeeInBP;
-        emit SetSaleFees(_saleFeeInBP);
-    }
-
-    /// @notice See {IMaze-addToWhitelist}
-    function addToWhitelist(address account) external whenNotPaused onlyOwner {
-        require(!isWhitelisted[account], "Maze: Account already whitelisted");
-        isWhitelisted[account] = true;
-        emit AddToWhitelist(account);
-    }
-
-    /// @notice See {IMaze-removeFromWhitelist}
-    function removeFromWhitelist(address account) external whenNotPaused onlyOwner {
-        require(isWhitelisted[account], "Maze: Account not whitelisted");
-        isWhitelisted[account] = false;
-        emit RemoveFromWhitelist(account);
-    }
-
-    /// @notice See {IMaze-addToPairlist}
-    function addToPairlist(address pair) external whenNotPaused onlyOwner {
-        require(!isSalePair[pair], "Maze: Pair already added");
-        isSalePair[pair] = true;
-        emit AddToPairlist(pair);
-    }
-
-    /// @notice See {IMaze-removeFromPairlist}
-    function removeFromPairlist(address pair) external whenNotPaused onlyOwner {
-        require(isSalePair[pair], "Maze: Pair not added");
-        isSalePair[pair] = false;
-        emit RemoveFromPairlist(pair);
-    }
-
-    function setSaleFeeReceiver(address receiver) external whenNotPaused onlyOwner {
-        require(receiver != address(0), "Maze: Cannot include zero address");
-        saleFeeReceiver = receiver;
-        emit SaleFeeReceiverChanged(receiver);
-    }
-
-    function setPoolFee(uint24 poolFee_) external whenNotPaused onlyOwner {
-        require(poolFee_ != 0, "Maze: Cannot be zero");
-        poolFee = poolFee_;
-        emit PoolFeeChanged(poolFee_);
-    }
-
     /// @notice See {IMaze-pause}
     function pause() external onlyOwner {
         _pause();
@@ -198,49 +117,6 @@ contract Maze is ERC20, IMaze, Ownable, Pausable {
         address to,
         uint256 amount
     ) internal override ifNotBlacklisted(from) ifNotBlacklisted(to) {
-        uint256 saleFeeAmount = 0;
-        if (isSalePair[to] && !isWhitelisted[from]) {
-            if (!inSwap)
-                saleFeeAmount = amount.mul(saleFeeInBP).div(percentConverter);
-        }
-        console.log("Sale fee amount", saleFeeAmount);
-        console.log("Amount: ", amount);
-        console.log("BalanceOf from: ", balanceOf(from));
-        console.log("Address from: ", from);
-        console.log("Address to: ", to);
-        uint256 amountWithFee = amount.add(saleFeeAmount);
-
-        require(balanceOf(from) >= amountWithFee, "Maze: not enough tokens to pay the fee");
-
-        console.log("Before transfer");
         super._transfer(from, to, amount);
-        console.log("After transfer");
-
-        if (saleFeeAmount > 0) {
-            console.log("Before fee transfer");
-            super._transfer(from, address(this), saleFeeAmount);
-            console.log("After fee transfer");
-            inSwap = true;
-            _processSaleFees(saleFeeAmount);
-            inSwap = false;
-        }
-    }
-
-    function _processSaleFees(uint256 saleFeeAmount) private {
-        _approve(address(this), address(swapRouter), saleFeeAmount);
-        console.log("BalanceOf address this: ", balanceOf(address(this)));
-        console.log("Approved: ", allowance(address(this), address(swapRouter)));
-        swapRouter.exactInputSingle(
-            ISwapRouter.ExactInputSingleParams({
-            tokenIn: address(this),
-            tokenOut: USDT,
-            fee: poolFee,
-            recipient: saleFeeReceiver,
-            deadline: block.timestamp,
-            amountIn: saleFeeAmount,
-            amountOutMinimum: 0,
-            sqrtPriceLimitX96: 0
-        }));
-        console.log("Swapped");
     }
 }
--- contracts/interfaces/IMaze.sol
+++ contracts/interfaces/IMaze.sol
@@ -6,47 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @notice Interface of the ERC20 standard as defined in the EIP.
 interface IMaze is IERC20 {
-    /// @notice Indicates that sale fee amount has been changed
-    event SetSaleFees(uint256 newSaleFeeAmount);
-
-    /// @notice Indicates that user has been added to whitelist
-    event AddToWhitelist(address account);
-
-    /// @notice Indicates that user has been removed from whitelist
-    event RemoveFromWhitelist(address account);
-
-    /// @notice Indicates that user has been added to sale pair list
-    event AddToPairlist(address pair);
-
-    /// @notice Indicates that user has been removed from sale pair list
-    event RemoveFromPairlist(address pair);
-
-    /// @notice Indicates that saleFeeReceiver address has been changed
-    event SaleFeeReceiverChanged(address receiver);
-
-    /// @notice Indicates that pool fee amount has been changed
-    event PoolFeeChanged(uint24 poolFee);
-
-    /// @notice Adds a user to the whitelist
-    ///         Whitelisted users do not pay fees
-    /// @param account Address of the user
-    function addToWhitelist(address account) external;
-
-    /// @notice Remove a user from the whitelist
-    ///         Whitelisted users do not pay fees
-    /// @param account Address of the user
-    function removeFromWhitelist(address account) external;
-
-    /// @notice Adds a user to the sale pair list
-    ///         When selling a token in a pair, a sale commission is taken
-    /// @param pair Address of the pair
-    function addToPairlist(address pair) external;
-
-    /// @notice Remove a user from the sale pair list
-    ///         When selling a token in a pair, a sale commission is taken
-    /// @param pair Address of the pair
-    function removeFromPairlist(address pair) external;
-
     /// @notice Pause the contract
     function pause() external;
 
